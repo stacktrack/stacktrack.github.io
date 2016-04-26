999976166f21963cad32d574299e2c51d132c310
Thomas Pollet <thomaspollet@gmail.com>
2016-04-26 12:05:28 -0700

    i

--- src/trace.sh
+++ src/trace.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
-#exec_slave(){
-alias sshu="ssh 'root@192.168.122.10'"
+#
+# This script creates a trace as follows:
+#   - Reset the VM snapshot
+#   - create gdb command files to be run on the master and the slave (VM)
+#   - start gdb on the master
+#   - ssh to the slave and start the test (trace-slave.sh)
+#
+# The gdb session on the master will set a breakpoint on SYSCALL (argv[1]).
+# When the breakpoint is hit by the program on the slave, nodes are added to
+# the trace. When the program exits, the nodes are dumped.
+#
 
 SYSCALL=$1
+VM=192.168.122.10
 
 if [[ -z $SYSCALL ]]
 then
@@ -14,7 +23,7 @@ fi
 virsh snapshot-revert tvm tvm-0
 
 GDBCMDS_MASTER=$(mktemp)
-GDBCMDS_SLAVE=/mnt/t/gdbcmds-client
+GDBCMDS_SLAVE=/mnt/t/gdbcmds-slave
 
 sed -e "s/_SYSCALL_/sys_$SYSCALL/g" gdbcmds > $GDBCMDS_MASTER
 sed -e "s/_SYSCALL_/$SYSCALL/g" gdbcmds-client > $GDBCMDS_SLAVE
@@ -23,14 +32,11 @@ gdb -q -x $GDBCMDS_MASTER &
 PID_GDB=$!
 
 sleep 20
-ssh 'root@192.168.122.10' /mnt/t/trace-client.sh &
+ssh root@$VM /mnt/t/trace-slave.sh &
 PID_SSH=$!
 
 (sleep 1800 ; kill -9 $PID_GDB $PID_SSH) &
 
 wait $PID_SSH
-
-#sshu cat /mnt/log
-#killall -9 gdb
 
 rm -f $GDBCMDS_MASTER $GDBCMDS_SLAVE
