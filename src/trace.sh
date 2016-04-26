9e533782a30f2d13649476a46742163f1d45b7de
Thomas Pollet <thomaspollet@gmail.com>
2016-04-26 11:53:23 -0700

    i

--- src/trace.sh
+++ src/trace.sh
@@ -1,12 +1,36 @@
-# Start vm if it's not yet started
+#!/bin/bash
 
-VM=tvm
-virsh list | grep -q "$VM" || virsh start "$VM"
+#exec_slave(){
+alias sshu="ssh 'root@192.168.122.10'"
 
-if [[ $? != 0 ]]
+SYSCALL=$1
+
+if [[ -z $SYSCALL ]]
 then
-    echo $VM not started
+    echo no syscall
     exit 1
 fi
 
-sshu
+virsh snapshot-revert tvm tvm-0
+
+GDBCMDS_MASTER=$(mktemp)
+GDBCMDS_SLAVE=/mnt/t/gdbcmds-client
+
+sed -e "s/_SYSCALL_/sys_$SYSCALL/g" gdbcmds > $GDBCMDS_MASTER
+sed -e "s/_SYSCALL_/$SYSCALL/g" gdbcmds-client > $GDBCMDS_SLAVE
+
+gdb -q -x $GDBCMDS_MASTER & 
+PID_GDB=$!
+
+sleep 20
+ssh 'root@192.168.122.10' /mnt/t/trace-client.sh &
+PID_SSH=$!
+
+(sleep 1800 ; kill -9 $PID_GDB $PID_SSH) &
+
+wait $PID_SSH
+
+#sshu cat /mnt/log
+#killall -9 gdb
+
+rm -f $GDBCMDS_MASTER $GDBCMDS_SLAVE
