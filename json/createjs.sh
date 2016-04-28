e31eb43292d67d6b8c8bfdc087c6eb7b8b789555
Thomas Pollet <thomaspollet@gmail.com>
2016-04-28 01:10:38 -0700

    autocommit

--- json/createjs.sh
+++ json/createjs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo 'var syscalls = ['
 ls | grep race | while read line
 do
     syscall=$(echo $line | cut -f2- -d_ | cut -f1 -d- | sed -e 's/_callees//g')
     echo "['$syscall','$line'],"
 done
+
+echo '];' 
